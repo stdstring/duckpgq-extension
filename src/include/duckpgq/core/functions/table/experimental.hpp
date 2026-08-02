93bbb461a77dbcdaa180ded579ca3ebf22d9f051
stdstring <std_string@mail.ru>
2026-08-03 00:47:00 +0500

    made several improvements

--- src/include/duckpgq/core/functions/table/experimental.hpp
+++ src/include/duckpgq/core/functions/table/experimental.hpp
@@ -7,14 +7,14 @@
 
 namespace duckdb {
 
+struct ExperimentalGlobalState : public GlobalTableFunctionState {
+    bool finished = false;
+};
+
 // get graph name
 struct GetGraphNameBindData : public TableFunctionData {
     string graph_name;
     bool graph_exists = false;
-};
-
-struct GetGraphNameGlobalState : public GlobalTableFunctionState {
-    bool finished = false;
 };
 
 class GetGraphNameFunction : public TableFunction {
@@ -27,12 +27,15 @@ public:
         function = Main;
     }
 
-    static unique_ptr<GlobalTableFunctionState> InitGlobal(ClientContext &context, TableFunctionInitInput &input) {
-        return make_uniq<GetGraphNameGlobalState>();
+    static unique_ptr<GlobalTableFunctionState> InitGlobal(ClientContext &context,
+                                                           TableFunctionInitInput &input) {
+        return make_uniq<ExperimentalGlobalState>();
     }
 
-    static unique_ptr<FunctionData> Bind(ClientContext &context, TableFunctionBindInput &input,
-                                         vector<LogicalType> &return_types, vector<string> &names) {
+    static unique_ptr<FunctionData> Bind(ClientContext &context,
+                                         TableFunctionBindInput &input,
+                                         vector<LogicalType> &return_types,
+                                         vector<string> &names) {
         return_types.push_back(LogicalType::VARCHAR);
         names.emplace_back("graph_name");
         auto result = make_uniq<GetGraphNameBindData>();
@@ -46,12 +49,14 @@ public:
         return std::move(result);
     }
 
-    static void Main(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
-        auto &state = data.global_state->Cast<GetGraphNameGlobalState>();
+    static void Main(ClientContext &context,
+                     TableFunctionInput &data,
+                     DataChunk &output) {
+        auto &state = data.global_state->Cast<ExperimentalGlobalState>();
         auto &bind_data = data.bind_data->Cast<GetGraphNameBindData>();
         if (state.finished) {
-          output.SetCardinality(0);
-          return;
+            output.SetCardinality(0);
+            return;
         }
         output.SetValue(0, 0, bind_data.graph_exists ? Value(bind_data.graph_name) : Value(LogicalType::VARCHAR));
         output.SetCardinality(1);
@@ -65,10 +70,6 @@ struct CountNodesBindData : public TableFunctionData {
     bool graph_exists = false;
 };
 
-struct CountNodesGlobalState : public GlobalTableFunctionState {
-    bool finished = false;
-};
-
 class CountNodesFunction : public TableFunction {
 public:
     CountNodesFunction() {
@@ -79,32 +80,37 @@ public:
         function = Main;
     }
 
-    static unique_ptr<FunctionData> Bind(ClientContext &context, TableFunctionBindInput &input,
-                                         vector<LogicalType> &return_types, vector<string> &names) {
+    static unique_ptr<GlobalTableFunctionState> InitGlobal(ClientContext &context,
+                                                           TableFunctionInitInput &input) {
+        return make_uniq<ExperimentalGlobalState>();
+    }
+
+    static unique_ptr<FunctionData> Bind(ClientContext &context,
+                                         TableFunctionBindInput &input,
+                                         vector<LogicalType> &return_types,
+                                         vector<string> &names) {
         return_types.push_back(LogicalType::BIGINT);
         names.emplace_back("node_count");
         auto result = make_uniq<CountNodesBindData>();
-        string graph_name = StringValue::Get(input.inputs[0]);
-        auto pgq_state = GetDuckPGQState(context);
-        auto graph_it = pgq_state->registered_property_graphs.find(graph_name);
+        const string graph_name = StringValue::Get(input.inputs[0]);
+        const auto pgq_state = GetDuckPGQState(context);
+        const auto graph_it = pgq_state->registered_property_graphs.find(graph_name);
         if (graph_it != pgq_state->registered_property_graphs.end()) {
             result->graph_exists = true;
-            auto graph = dynamic_cast<CreatePropertyGraphInfo*>(graph_it->second.get());
+            const auto graph = dynamic_cast<CreatePropertyGraphInfo*>(graph_it->second.get());
             auto &catalog = Catalog::GetCatalog(context, INVALID_CATALOG);
-            for (auto &vertex_table : graph->vertex_tables) {
+            for (const auto &vertex_table : graph->vertex_tables) {
                 auto &table_entry = catalog.GetEntry<TableCatalogEntry>(context, DEFAULT_SCHEMA, vertex_table->table_name);
-                result->node_count += table_entry.GetStorage().GetTotalRows();
+                result->node_count += static_cast<int64_t>(table_entry.GetStorage().GetTotalRows());
             }
         }
         return std::move(result);
     }
 
-    static unique_ptr<GlobalTableFunctionState> InitGlobal(ClientContext &context, TableFunctionInitInput &input) {
-        return make_uniq<CountNodesGlobalState>();
-    }
-
-    static void Main(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
-        auto &state = data.global_state->Cast<CountNodesGlobalState>();
+    static void Main(ClientContext &context,
+                     TableFunctionInput &data,
+                     DataChunk &output) {
+        auto &state = data.global_state->Cast<ExperimentalGlobalState>();
         auto &bind_data = data.bind_data->Cast<CountNodesBindData>();
         if (state.finished) {
             output.SetCardinality(0);
